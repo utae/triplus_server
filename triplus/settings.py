9e4df05cd145ee6514e87e6e43802b4cad49552e
utae <utae@yeou.us>
2019-04-25 16:18:38 +0900

    db set strict mode

--- triplus/settings.py
+++ triplus/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         'PASSWORD': 'globetrip12+',
         'HOST': '',
         'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
 
     }
 }
