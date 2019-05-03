99b1ab5a05cfee4092a395f7f5a31617289e4cc9
utae <utae@yeou.us>
2019-05-03 16:01:24 +0900

    split the settings.py. one is for development, the other is for production

--- triplus/settings/base.py
+++ triplus/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -22,11 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '42*088kn)7ttpje&ifwb(^i8#d0-&^cd8cy=5ff%zf6hjdj8@&'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
 ALLOWED_HOSTS = '*'
-
 
 # Application definition
 
@@ -73,30 +69,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'triplus.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'triplus',
-        'USER': 'root',
-        'PASSWORD': 'globetrip12+',
-        'HOST': '',
-        'PORT': '',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-            'charset': 'utf8mb4',
-            'use_unicode': True,
-        },
-
-    }
-}
 
 
 # Password validation
---
+++ triplus/settings/production.py
@@ -0,0 +1,22 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Database
+# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'triplus',
+        'USER': 'root',
+        'PASSWORD': 'globetrip12+',
+        'HOST': '',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+            'use_unicode': True,
+        },
+    }
+}
